75cea7df7bf2992a3bb7bc07fe7af49e3e7f0dda
Melissa Julia L. L <melissa.lequipe@gmail.com>
2023-09-02 15:16:45 -0300

    Update mostrar-media.js

--- js/mostrar-media.js
+++ js/mostrar-media.js
@@ -1,7 +1,7 @@
 // const primeira = document.querySelector('.primeira');
 // const segunda = document.querySelector('.segunda');
 // const media = document.querySelector('.media');
-// const situacao = document.querySelector('.situacao');
+const situacao = document.querySelector('.situacao');
 
 primeira.oninput = showValue;
 segunda.oninput = showValue;
@@ -9,19 +9,15 @@ segunda.oninput = showValue;
     function showValue(){
         media.value = ((parseFloat(primeira.value) + parseFloat(segunda.value))/2).toFixed(2);
     }
-
+const med = media.value
     
-let a = "Aprovado";
-let b = "Recuperação";
-let c = "Reprovado";
- 
-media.oninput  = function(){
-    if(this.value >=7){
-        addTosituacao(a);
-    }else if (media.value >=5){
-        addTosituacao(b);
+media.onChange = function(){
+    if(med >=7){
+        situacao.value = "Aprovado";
+    }else if (med >=5){
+        situacao.value = "Recuperação";
     }else{
-        addTosituacao(c);
+        situacao.value = "Reprovado";
     }
 }
 
